e19730d18d792594560ca25409b8accbea8ec548
owaiskamal <owaiskamal12@gmail.com>
2017-05-12 23:45:08 +0500

    Add files via upload

---
+++ Import Data App/Import Data App/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Import_Data_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xGrid = new System.Windows.Forms.DataGridView();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblLabel = new System.Windows.Forms.Label();
+            this.picbox = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.xGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xGrid
+            // 
+            this.xGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.xGrid.Location = new System.Drawing.Point(12, 12);
+            this.xGrid.Name = "xGrid";
+            this.xGrid.RowTemplate.Height = 24;
+            this.xGrid.Size = new System.Drawing.Size(900, 498);
+            this.xGrid.TabIndex = 0;
+            this.xGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.xGrid_CellClick);
+            this.xGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.xGrid_CellContentClick);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(824, 537);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(733, 536);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "READ";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lblLabel
+            // 
+            this.lblLabel.AutoSize = true;
+            this.lblLabel.Location = new System.Drawing.Point(12, 524);
+            this.lblLabel.Name = "lblLabel";
+            this.lblLabel.Size = new System.Drawing.Size(74, 17);
+            this.lblLabel.TabIndex = 3;
+            this.lblLabel.Text = "Total Data";
+            // 
+            // picbox
+            // 
+            this.picbox.Location = new System.Drawing.Point(939, 41);
+            this.picbox.Name = "picbox";
+            this.picbox.Size = new System.Drawing.Size(232, 232);
+            this.picbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picbox.TabIndex = 4;
+            this.picbox.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(946, 299);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(46, 17);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "label1";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(946, 345);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(46, 17);
+            this.lblPhone.TabIndex = 6;
+            this.lblPhone.Text = "label2";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(946, 385);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(46, 17);
+            this.lblEmail.TabIndex = 7;
+            this.lblEmail.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 601);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.picbox);
+            this.Controls.Add(this.lblLabel);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.xGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.xGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView xGrid;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblLabel;
+        private System.Windows.Forms.PictureBox picbox;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
+
---
+++ Import Data App/Import Data App/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Import_Data_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            xGrid.Columns.Add("A", "SID");
+            xGrid.Columns.Add("B", "NAME");
+            xGrid.Columns.Add("C", "PHONE");
+            xGrid.Columns.Add("D", "EMAIL");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            StreamReader xReader = new StreamReader("OurData.txt");
+            string[] Arr = new string[4];
+            while(!xReader.EndOfStream)
+            {
+                Arr = xReader.ReadLine().Split('-');
+                xGrid.Rows.Add(Arr[0], Arr[1], Arr[2], Arr[3]);
+            }
+            xReader.Close();
+            lblLabel.Text = "Total Records:" + xGrid.Rows.Count;
+        }
+
+        private void xGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void xGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            string SID = xGrid.CurrentRow.Cells[0].Value.ToString();
+            lblName.Text= xGrid.CurrentRow.Cells[1].Value.ToString();
+            lblPhone.Text= xGrid.CurrentRow.Cells[2].Value.ToString();
+            lblEmail.Text= xGrid.CurrentRow.Cells[3].Value.ToString();
+            picbox.Load("images//" + SID + ".png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
